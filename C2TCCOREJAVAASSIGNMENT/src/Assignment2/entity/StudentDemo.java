2215e1a78983ff3279cf3fe52d001eb8d4530765
haswinhaswin014@gmail.com <aswin@192.168.1.33>
2025-07-23 22:30:48 +0530

    Assignment 2

---
+++ C2TCCOREJAVAASSIGNMENT/src/Assignment2/entity/StudentDemo.java
@@ -0,0 +1,15 @@
+package Assignment2.entity;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+	Student s=new Student();
+	s.setName("Aswin");
+	s.setAdress("no: 23,FF Street");
+	s.setPhone(455634241);
+	s.setSales_amount(346788);
+	s.setCommission(6);
+	s.calculateCommission();
+	}
+
+}
